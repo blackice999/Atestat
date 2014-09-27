782377a7c422709deec63402cc17a88b90bcb555
blackice999 <adam596341@yahoo.com>
2014-09-27 20:07:44 +0300

    Removed foreign keys

--- sql/user_address.sql
+++ sql/user_address.sql
@@ -1,14 +1,9 @@
 CREATE TABLE IF NOT EXISTS `user_address`
 (
     `ID` INT(4) NOT NULL AUTO_INCREMENT,
-    `user_ID` INT(4) NOT NULL,
     `city` VARCHAR(50) NOT NULL,
     `street` VARCHAR(50) NOT NULL,
     `zip` INT(9) NOT NULL,
     `country` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`ID`) 
-    FOREIGN KEY (`user_ID`)
-        REFERENCES `user`(`ID`)
-        ON DELETE CASCADE
-
+    PRIMARY KEY (`ID`)
 )
--- sql/user_status.sql
+++ sql/user_status.sql
@@ -1,10 +1,6 @@
 CREATE TABLE IF NOT EXISTS `user_status`
 (
     `ID` INT(4) NOT NULL AUTO_INCREMENT,
-    `user_ID` INT(4) NOT NULL,
     `status` VARCHAR(20) NOT NULL,
     PRIMARY KEY (`ID`)
-    FOREIGN KEY (`status`, `user_ID`)
-        REFERENCES `user`(`status`, `ID`)
-        ON DELETE CASCADE		
 )
