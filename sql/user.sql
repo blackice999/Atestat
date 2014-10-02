d63385f820e12457a4450c7d6e887ac9b5b504d3
blackice999 <adam596341@yahoo.com>
2014-10-02 16:01:35 +0300

    Added foreign key to user_address table

--- sql/user.sql
+++ sql/user.sql
@@ -5,12 +5,8 @@ CREATE TABLE IF NOT EXISTS `user`
     `statusID` INT(4) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `password_hash` VARCHAR(255) NOT NULL,
-    `addressID` INT(4) NOT NULL,
     PRIMARY KEY (`ID`)
 
     FOREIGN KEY(`statusID`)
         REFERENCES user_status(`ID`)
-
-    FOREIGN KEY(`addressID`)
-        REFERENCES user_address(`ID`)
 ) ENGINE=INNODB;
--- sql/user_address.sql
+++ sql/user_address.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS `user_address`
 (
     `ID` INT(4) NOT NULL AUTO_INCREMENT,
+    `userID` INT(4) NOT NULL,
     `city` VARCHAR(50) NOT NULL,
     `street` VARCHAR(50) NOT NULL,
     `zip` INT(9) NOT NULL,
     `country` VARCHAR(50) NOT NULL,
     PRIMARY KEY (`ID`)
+
+    FOREIGN KEY (`user_ID`)
+        REFERENCES `user`(`ID`)
 ) ENGINE=INNODB;
