7ffa58879c306536c0ce534ed3ac30b6b29fd43e
blackice999 <adam596341@yahoo.com>
2014-10-03 15:59:56 +0300

    Added missing commas

--- sql/user_address.sql
+++ sql/user_address.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `user_address`
     `street` VARCHAR(50) NOT NULL,
     `zip` INT(9) NOT NULL,
     `country` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`ID`)
+    PRIMARY KEY (`ID`),
 
     FOREIGN KEY (`user_ID`)
         REFERENCES `user`(`ID`)
