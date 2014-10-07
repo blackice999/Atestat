9b9ec3558f3c3cf20bcc9081e8afd69a276b3004
blackice999 <adam596341@yahoo.com>
2014-10-07 20:27:32 +0300

    Removed extra dash

--- sql/user_address.sql
+++ sql/user_address.sql
@@ -11,6 +11,6 @@ CREATE TABLE IF NOT EXISTS `user_address`
     `country` VARCHAR(50) NOT NULL,
     PRIMARY KEY (`ID`),
 
-    FOREIGN KEY (`user_ID`)
+    FOREIGN KEY (`userID`)
         REFERENCES `user`(`ID`)
 ) ENGINE=INNODB;
