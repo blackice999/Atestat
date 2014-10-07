5ca4ff984842ee183338cfb9a8d47829bfeb182f
blackice999 <adam596341@yahoo.com>
2014-10-07 20:28:16 +0300

    Added fetch row and fetch object

--- app/database/layer.php
+++ app/database/layer.php
@@ -56,6 +56,8 @@
         */
         public function __construct()
         {
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
             $this->database = new mysqli(
                 $this->databaseHost,
                 $this->databaseUser,
@@ -84,6 +86,150 @@
             $this->database = NULL;
         }
 
-        
+        /**
+         * Fetches the ID, email and date_registered rows from user table
+         * as an enumerated array
+         * @param  integer $ID
+         * @return resource
+         */
+        public function fetchRowUser($ID)
+        {
+            $query = 'SELECT `ID`,`email`, `date_registered` FROM `user` WHERE = ?';
+            $stmt = $this->database->prepare($query);
+            $stmt = $this->database->bind_param('i', $ID);
+
+            try
+            {
+                $stmt->execute();
+
+                if ($stmt->execute())
+                {
+                    while ($row = $stmt->fetch_row)
+                    {
+                        printf ("%s (%s)\n",
+                         $row[0],
+                         $row[1],
+                         $row[2]
+                        );
+                    }
+                }
+            }
+
+            catch(Exception $e)
+            {
+                return $this->monolog->addError('Error:' . $this->database->error);
+            }
+        }
+
+        /**
+         * Fetches ID, userID, city, street, zip, country from user_address table
+         * as an enumerated array
+         * @param  integer $ID
+         * @return resource
+         */
+        public function fetchRowUser_address($ID)
+        {
+            $query = 'SELECT `ID`,`userID`, `city`, `street`, `zip`,`country` FROM `user_address` WHERE = ?';
+            $stmt = $this->database->prepare($query);
+            $stmt = $this->database->bind_param('i', $ID);
+
+            try
+            {
+                $stmt->execute();
+
+                if ($stmt->execute())
+                {
+                    while ($row = $stmt->fetch_row)
+                    {
+                        printf ("%s (%s)\n",
+                         $row[0],
+                         $row[1],
+                         $row[2],
+                         $row[3],
+                         $row[4],
+                         $row[5]
+                        );
+                    }
+                }
+            }
+
+            catch(Exception $e)
+            {
+                return $this->monolog->addError('Error:' . $this->database->error);
+            }
+        }
+
+        /**
+         * Fetches ID, email, date_registered from user table
+         * as an object
+         * @param  integer $ID
+         * @return resource
+         */
+        public function fetchObjectUser($ID)
+        {
+            $query = 'SELECT `ID`,`email`, `date_registered` FROM `user` WHERE = ?';
+            $stmt = $this->database->prepare($query);
+            $stmt = $this->database->bind_param('i', $ID);
+
+            try
+            {
+                $stmt->execute();
+
+                if ($stmt->execute())
+                {
+                    while ($obj = $stmt->fetch_object)
+                    {
+                        printf ("%s (%s)\n",
+                         $obj->ID,
+                         $obj->email,
+                         $obj->date_registered);
+                    }
+                }
+            }
+
+            catch(Exception $e)
+            {
+                return $this->monolog->addError('Error:' . $this->database->error);
+            }
+        }
+
+        /**
+         * Fetches ID, userID, city, street, zip, country from user_address
+         * as an object
+         * @param  intger $ID
+         * @return resource
+         */
+        public function fetchObjectUser_address($ID)
+        {
+            $query = 'SELECT `ID`,`userID`, `city`, `street`, `zip`,`country` FROM `user_address` WHERE = ?';
+            $stmt = $this->database->prepare($query);
+            $stmt = $this->database->bind_param('i', $ID);
+
+            try
+            {
+                $stmt->execute();
+
+                if ($stmt->execute())
+                {
+                    while ($row = $stmt->fetch_row)
+                    {
+                        printf ("%s (%s)\n",
+                         $obj->ID,
+                         $obj->userID,
+                         $obj->city,
+                         $obj->street,
+                         $obj->zip,
+                         $obj->country
+                        );
+                    }
+                }
+            }
+
+            catch(Exception $e)
+            {
+                return $this->monolog->addError('Error:' . $this->database->error);
+            }
+        }
+
     }
  ?>
