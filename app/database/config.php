34e42064d26663015378b94da161fa16a6057963
blackice999 <adam596341@yahoo.com>
2014-10-10 20:03:05 +0300

    Added class description

--- app/database/config.php
+++ app/database/config.php
@@ -3,6 +3,9 @@
 	error_reporting(E_ALL);
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
+    /**
+     * This class holds the database connection info
+     */
     class Config
     {
         /**
