d2d6a1cb6016a0e210bf100a065cc485eeb6e142
blackice999 <adam596341@yahoo.com>
2014-10-12 19:40:34 +0300

    Added Memcached

--- app/database/config.php
+++ app/database/config.php
@@ -3,6 +3,9 @@
 	error_reporting(E_ALL);
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
+    $memcached = new Memcached();
+    $memcached->addServer('127.0.0.1', '11211');
+
     /**
      * This class holds the database connection info
      */
