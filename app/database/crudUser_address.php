c22cc1f0e6b1def0b482e66e4da1814c52b97613
blackice999 <adam596341@yahoo.com>
2014-10-18 19:20:02 +0300

    Added fetchUser_address and renamed to getUser_address_ID

--- app/database/crudUser_address.php
+++ app/database/crudUser_address.php
@@ -132,6 +132,84 @@
         }
 
         /**
+         * Fetches ID, userID, city, street, zip, country from user_address table
+         * as an enumerated array if $type is 'row'
+         * or as an object if $type is 'object'
+         * @param  integer $ID
+         * @param string $type Holds which way to fetch the data
+         * @return resource
+         */
+        public function getUser_addressData_ID($ID, $type = 'row')
+        {
+            try
+            {
+                $user = NULL;
+
+                //Get user address with id $ID
+                if ($this->memcached)
+                {
+                    $key = 'user_' . $ID;
+                    $user = $this->memcached->get($key);
+                }
+
+                //If the cache can't be accessed, get the data from mysql
+                if (!$user)
+                {
+
+                    $query = 'SELECT `ID`,`userID`, `city`, `street`, `zip`,`country`
+                    FROM `user_address`
+                    WHERE `ID`= ?';
+
+                    $stmt = $this->database->stmt_init();
+
+                    if($stmt->prepare($query))
+                    {
+                        $stmt->bind_param('i', $ID);
+
+                        $stmt->execute();
+
+                        $result = $stmt->get_result();
+
+                        if ($type == 'row')
+                        {
+                            while ($row = $result->fetch_array(MYSQLI_NUM))
+                            {
+                                printf ("%s (%s) %s %s %s\n",
+                                 $row[0],
+                                 $row[1],
+                                 $row[2],
+                                 $row[3],
+                                 $row[4],
+                                 $row[5]
+                                );
+                            }
+                        }
+
+                        elseif ($type == 'object')
+                        {
+                            while ($row = $result->fetch_array(MYSQLI_ASSOC))
+                            {
+                                printf ("%s (%s) %s %s %s\n",
+                                 $row['ID'],
+                                 $row['userID'],
+                                 $row['city'],
+                                 $row['street'],
+                                 $row['zip'],
+                                 $row['country']
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch(Exception $e)
+            {
+                $this->generateLogMysql();
+            }
+        }
+
+        /**
          * Updates the user's address userID with $userID parameter
          * @param  int $ID     The row ID
          * @param  int $userID The userID from 'user' table
