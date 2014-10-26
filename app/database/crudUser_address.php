282494fcb5649b8941112b81f67b9e680dfff1ef
blackice999 <adam596341@yahoo.com>
2014-10-26 20:00:03 +0200

    Added crudUser file

--- app/database/crudUser_address.php
+++ app/database/crudUser_address.php
@@ -1,6 +1,7 @@
 <?php 
     require __DIR__. '/../../vendor/autoload.php';
     require __DIR__. '/config.php';
+    require __DIR__. '/crudUser.php';
 
     use Monolog\Logger;
     use Monolog\Handler\StreamHandler;
