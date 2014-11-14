6ed529c93f13516bef8ecb9979dc510afee2c163
blackice999 <adam596341@yahoo.com>
2014-11-14 19:59:23 +0200

    Displayed info from user address table and user status

--- members.php
+++ members.php
@@ -19,27 +19,33 @@
 
     //Runs a prepared query to get info from 'user' table
     $query_user = $db->bindQuery(
-        "SELECT `email`, `statusID`, `date_registered` FROM `user` WHERE `ID` = ?",
+        "SELECT `email`, `user_status`.`status`, `date_registered`
+         FROM `user`
+         INNER JOIN `user_status`
+         ON `user`.`statusID` = `user_status`.`ID` WHERE `user`.`ID` = ?",
         $bindArray);
 
-    //Get the result of the query
-    // $info_user = $db->getArray($query_user, MYSQLI_ASSOC);
-
-    //Runs a prepared query to get info from 'user_address' table
-    $query_address = $db->bindQuery(
-        "SELECT `userID`, `city`, `street`, `zip`, `country` FROM `user_address` WHERE `userID` = ?",
-        $bindArray);
-
-    //Get the result of the query
-    $info_address = $db->getArray($query_address, MYSQLI_ASSOC);
-
-    //Runs a query to get table info
+    //Runs a query to get 'user' table info
     $field = $db->runQuery(
         "SHOW COLUMNS FROM `user` WHERE Field NOT IN ('password','password_hash')",
         array());
 
     //Get the result of the query
-    $field_array = $db->getArray($field)
+    $field_array = $db->getArray($field);
+
+    //Runs a prepared query to get info from 'user_address' table
+    $query_address = $db->bindQuery(
+        "SELECT `city`, `street`, `zip`, `country` FROM `user_address` WHERE `userID` = ?",
+        $bindArray);
+
+    //Runs a query to get 'user_address' table info
+    $field_address = $db->runQuery(
+        "SHOW COLUMNS FROM `user_address` WHERE Field NOT IN ('userID')",
+        array()
+        );
+
+    //Get the result of the query - necessary, because it doesn't include `ID` column
+    $info_address = $db->getArray($field_address);
 ?>
 <!DOCTYPE html>
 <html>
@@ -51,14 +57,26 @@
     <a href="logout.php" id="logout-right">Log out </a>
     <div id="container">
  
-       <table id="user_info" cellpadding="10">
+       <table class="user_info" cellpadding="10">
      
             <thead>
            <?php  while ($row = $db->getArray($field)): ?>
 
                 <tr>
                     <th>
-                        <?php echo $row[0]; ?>
+                        <?php
+                            if ($row[0] == 'statusID')
+                            {
+                                $row[0] = 'status';
+                            }
+
+                            elseif ($row[0] == 'date_registered')
+                            {
+                                $row[0] = 'date registered';
+                            }
+
+                            echo ucfirst($row[0]);
+                         ?>
                     </th>
                 </tr>
 
@@ -68,7 +86,7 @@
 
             <tbody>
 
-                <?php while($info_user = $db->getArray($query_user, MYSQLI_ASSOC)): ?>
+                <?php while($info_user = $db->getArray($query_user)): ?>
 
                      <?php foreach ($info_user as $info): ?>
                         <tr>
@@ -78,6 +96,34 @@
                      <?php endforeach; ?>
                 <?php endwhile; ?>
             </tbody>
+        </table>
+
+        <h2> Address</h2>
+        <table class="user_info" cellpadding="10">
+            <thead>
+                <?php while ($row_address = $db->getArray($field_address)): ?>
+
+                    <tr>
+                        <th>
+                            <?php echo ucfirst($row_address[0]); ?>
+                        </th>
+                    </tr>
+
+                <?php endwhile; ?>
+
+            </thead>
+
+            <tbody>
+                <?php while ($info_address = $db->getArray($query_address)): ?>
+                    <?php foreach ($info_address as $info): ?>
+                        <tr>
+                            <td> <?php echo $info; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endwhile; ?>
+
+            </tbody>
+            
         </table>
     </div>
 </body>
