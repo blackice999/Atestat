613583b3d5db4b4f7c73ebd9f8d504fd1584e5b7
blackice999 <adam596341@yahoo.com>
2014-11-17 19:04:29 +0200

    Removed comments and added city,zup etc fields to register

--- index.php
+++ index.php
@@ -1,33 +1,75 @@
 <?php
-    require __DIR__. '/app/database/config.php';
-    echo "index.php";
+    require __DIR__. '/app/database/database.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $site_name; ?></title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <div id="banner">
+        <img src="design/accountsmall.jpg" width="100" height="50">
+        <span>Cool site name</span>
+    </div>
+    
+    <table class="form-middle" >
+        <form action = 'login.php' method = 'post'>
+            <tr class='align-left'>
+                <td>Email:</td>
+                <td><input type = 'text' name = 'Login[email]' id = 'email'/></td>
+            </tr>
 
-    echo "\n\r";
+            <tr class='align-left'>
+                <td>Password: </td>
+                <td><input type = 'password' name = 'Login[pass]' id = 'pass'/></td>
+            </tr>
 
-    $config = new Database();
+            <tr>
+                <td colspan="2"><input type = 'submit' value = 'Login' class="align-center"></td>
+            </tr>
 
-echo "<pre>";
-    var_dump($config);
-    echo "</pre>";
+        </form>
+    </table>
 
-    // $user = $config->runQuery("SELECT `email` from `user` where `ID` = 1",array());
-    // $row = $config->getArray();
-    // printf("%s", $row[0]);
 
-    echo "<br />";
+    <table class="form-middle" >
+        <form action = 'register.php' method = 'post'>
+            <tr class='align-left'>
+                <td>Email:</td>
+                <td><input type = 'text' name = 'Register[email]' id = 'email'/></td>
+            </tr>
 
-    $data = $config->runQuery("SELECT `email` from `user` where `ID` = 1", array());
+            <tr class='align-left'>
+                <td>Password: </td>
+                <td><input type = 'password' name = 'Register[pass]' id = 'pass'/></td>
+            </tr>
 
-    $row1 = $config->getArray($data);
+            <tr class='align-left'>
+                <td>City:</td>
+                <td><input type='text' name='Register[city]' id='city' /></td>
+            </tr>
 
-    $ID = 1;
-    $bindArray = array(
-        'bindTypes' => 'i',
-        'bindVariables' => $ID
-        );
-    $bind = $config->bindQuery("SELECT `email` from `user` where `ID` = ?", $bindArray);
-    $row2 = $config->getArray($bind);
-    printf("%s", $row2[0]);
+            <tr class='align-left'>
+                <td>Street:</td>
+                <td><input type='text' name='Register[street]' id='street' /></td>
+            </tr>
 
-    $update = $config->updateQuery("UPDATE `user` SET `email` = 'pista@something.net' WHERE `ID` = 1", array());
+             <tr class='align-left'>
+                <td>Zip:</td>
+                <td><input type='text' name='Register[zip]' id='zip' /></td>
+            </tr>
 
+             <tr class='align-left'>
+                <td>Country:</td>
+                <td><input type='text' name='Register[country]' id='country' /></td>
+            </tr>
+
+            <tr>
+                <td colspan="2"><input type = 'submit' value = 'Register' class="align-center"></td>
+            </tr>
+
+        </form>
+    </table>
+</body>
+</html>
