44828c5ea75f62d1cb4e8cd8077f8bfe490f1512
blackice999 <adam596341@yahoo.com>
2014-11-20 19:55:51 +0200

    Added comments

--- register.php
+++ register.php
@@ -2,9 +2,10 @@
     require __DIR__. '/app/database/database.php';
     require __DIR__.'/app/database/register.php';
 
+    //Prints an error if the redirect didn't come from index.php's form
     if (!isset($_POST['Register']))
     {
-        throw new  Exception("Unauthorized access!");       
+        throw new Exception("Unauthorized access!");
     }
 
      $db = new Database();
@@ -23,18 +24,22 @@
         $errors[] = 'Please enter a correct e-mail address';
      }
 
+     //If there are no errors, proceed by inserting the new user
      if (empty($errors))
      {
+
         $register = new Register();
+
         // put POST's contents into the register class's variables; 
         // $register->attributes = $_POST['Register'];
         foreach ($_POST['Register'] as $key => $value)
         {
             $register->$key = $value;
         } 
+        
+        // $insert = $register->doRegister($register->email, 1, $register->password);
 
-        // $insert = $register->doRegister($email, 1, $password);
-
+        //TO FIX -- INSERT A NEW USER IN USER AND USER_ADDRESS
         if ($insert)
         {
             echo "Registration successful. <a href='index.php'> Go back </a>";
@@ -46,12 +51,15 @@
         }
      }
 
+     //If there are errors, print them
      if (isset($errors))
      {
         foreach ($errors as $error)
         {
             echo $error;
         }
+
+        //Also create a back link to index.php
         echo "<a href='index.php'> Go back </a>";
      }
 ?>
