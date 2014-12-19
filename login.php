7a3732b725f7b2780bb291d2c0cd672ca3024cf9
blackice999 <adam596341@yahoo.com>
2014-12-19 19:14:52 +0200

    Moved to the end of file

--- login.php
+++ login.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $site_name; ?> - Login</title>
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
-<body>
-    <div id="banner">
-        <img src="design/logo_four.png">
-        <!-- <span><?php echo $site_name; ?></span> -->
-    </div>
-</body>
-</html>
 <?php 
     session_start();
     require __DIR__. '/app/database/database.php';
@@ -36,3 +23,16 @@ else
     echo "Incorrect username or password. <a href='index.php'> Go back </a>";
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $site_name; ?> - Login</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+     <div id="banner">
+        <img src="design/logo_four.png">
+        <!-- <span><?php echo $site_name; ?></span> -->
+    </div>
+</body>
+</html>
