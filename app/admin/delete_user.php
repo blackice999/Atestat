0e7fe40b9171bc4fc986130c3ce5fe77ac1e9144
blackice999 <adam596341@yahoo.com>
2014-12-23 17:28:10 +0200

    Added errors if the id is missing, or noone is logged in

--- app/admin/delete_user.php
+++ app/admin/delete_user.php
@@ -1,5 +1,19 @@
 <?php
-    require __DIR__ . '/../database/database.php'; 
+    require __DIR__ . '/../database/database.php';
+    session_start();
+
+    //Throws an error if there isn't a logged person,
+    //and he isn't an admin (having ID 1)
+    if (!(isset($_SESSION['id']) && $_SESSION['id'] == 1))
+    {
+        throw new Exception("Unauthorized access!");       
+    } 
+
+    if (!isset($_GET['id']))
+    {
+        throw new Exception("Missing ID");
+    }
+
     $id = $_GET['id'];
 
     echo $id;
