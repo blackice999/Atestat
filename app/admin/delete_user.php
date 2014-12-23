8bd88b116e3fd74148ffe9903aad257f61fa7de5
blackice999 <adam596341@yahoo.com>
2014-12-23 19:16:52 +0200

    Added logic to delete user and to redirect to display_users

--- app/admin/delete_user.php
+++ app/admin/delete_user.php
@@ -14,9 +14,24 @@
         throw new Exception("Missing ID");
     }
 
-    $id = $_GET['id'];
+    //Sanitize the received ID
+    //removing all characters except digits, plus and minus sign
+    $filter_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-    echo $id;
+    $db = new Database();
+
+    $bindArray = array(
+        'bindTypes' => 'i',
+        'bindVariables' => array(&$filter_id)
+        );
+
+    $delete_user = $db->bindQuery("DELETE FROM `user_address` WHERE `userID` = ?", $bindArray);
+    $delete_address = $db->bindQuery("DELETE FROM `user` WHERE `ID` = ? ", $bindArray);
+    if ($delete_user && $delete_address)
+    {
+        header("Location: display_users.php?action=deleted");
+        die();
+    }
 ?>
 
 <!DOCTYPE html>
