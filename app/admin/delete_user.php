1593a4a84dc2e950a9a834642d221521cd99e68b
blackice999 <adam596341@yahoo.com>
2014-12-23 19:34:04 +0200

    Removed HTML because it won't show on deletion

--- app/admin/delete_user.php
+++ app/admin/delete_user.php
@@ -33,22 +33,3 @@
         die();
     }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $site_name;?> - Delete user</title>
-    <link rel="stylesheet" type="text/css" href="../../css/main.css">
-</head>
-<body>
-    <div id="banner">
-        <img src="../../design/logo_four.png">
-        <!-- <span><?php echo $site_name; ?></span> -->
-    </div>
-
-    <div id="navigation">
-         <a href="../../logout.php" id="logout-right">Log out </a>
-         <a href="display_users.php" id="logout-right"> Go back </a>
-    </div>
-</body>
-</html>
