2f0e872174d83ece96b0b0c969cf4621b5aaaa0f
blackice999 <adam596341@yahoo.com>
2014-12-28 20:26:22 +0200

    Added showing only one list element, with its popup box

--- js/general.js
+++ js/general.js
@@ -26,23 +26,51 @@ $(document).ready(function() {
 //Opens or closes the pop-up box on display_users.php file
 //when the edit image is clicked
 function openPopup() {
-        document.getElementById('show-popup').style.display = 'block';
 
-        //If the browser width is less than 700px
-        //Hide the add user form
-        if (window.innerWidth < 700)
-        {
-            document.getElementById('info-right-display').style.display = 'none';
-        }
+    //On edit icon click
+    $('.registered-users').click(function(){
+
+        //Add class 'not-selected' to every list element
+        //except the selected one
+        //By this, only one list element (the selected) will be visible
+        $('.registered-users').not(this).addClass( 'not-selected' );
+
+        //If the numbering needs to be disabled, the below line does that
+        // $('.show-users').css('list-style-type','none');
+        
+        //Show the currently selected list element popup box
+        $('.registered-users #show-popup').show().index(this);
+    });
+
+    //If the browser width is less than 700px
+    //Hide the add user form
+    if (window.innerWidth < 700)
+    {
+        document.getElementById('info-right-display').style.display = 'none';
     }
+}
 
-    function closePopup() {
-        document.getElementById('show-popup').style.display = 'none';
+function closePopup() {
 
-        //If the browser width is less than 700px
-        //Showw the add user form
-        if (window.innerWidth < 700)
-        {
-            document.getElementById('info-right-display').style.display = 'block';
-        }
+    //On edit icon click
+    $('.registered-users').click(function(){
+
+        //Remove class 'not-selected' from every list element
+        //except the selected one
+        //By this, every list element will have its initial class structure
+        $('.registered-users').not(this).removeClass( 'not-selected' );
+
+        //If the numbering needs to be enabled, the below line does that
+        // $('.registered-users').css('list-style-type','decimal');
+        
+        // Hide the currently selected list element popup box
+        $('.registered-users #show-popup').hide().index(this);
+    });
+
+    //If the browser width is less than 700px
+    //Showw the add user form
+    if (window.innerWidth < 700)
+    {
+        document.getElementById('info-right-display').style.display = 'block';
     }
+}
