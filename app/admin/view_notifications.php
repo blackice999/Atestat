8578b3ff8eb6d15bbafc6c3be747648c1d737adc
blackice999 <adam596341@yahoo.com>
2015-01-03 19:51:24 +0200

    Add retrieving of columns from user_status table

--- app/admin/view_notifications.php
+++ app/admin/view_notifications.php
@@ -3,6 +3,23 @@
     {
         throw new Exception("Unauthorized access!");
     }
+
+    //Stores which variables should be binded to
+    $bindArray = array(
+        'bindTypes' => 'i',
+        'bindVariables' => array(&$users_array[0])
+        );
+
+    $query_status = $db->bindQuery("SELECT `status` FROM `user`
+        INNER JOIN `user_status`
+        ON `user`.`statusID` = `user_status`.`ID` WHERE `user`.`ID` = ?",
+        $bindArray);
+
+    $field = $db->runQuery(
+        "SHOW COLUMNS FROM `user_status`",
+        array());
+
+    $field_array = $db->getArray($field);
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +29,17 @@
     <link rel="stylesheet" type="text/css" href="../../css/main.css">
 </head>
 <body>
+    <table class="user_info" cellpadding="10">
+        <thead>
+            <?php while ($row = $db->getArray($field)): ?>
+                <tr>
+                    <th>
+                        <?php echo ucfirst($row[0]); ?>
+                    </th>
+                </tr>
+            <?php endwhile; ?>
+        </thead>
 
+    </table>
 </body>
 </html>
