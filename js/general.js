0cbdd257532620354a17e0ee90969dd2535cd80e
blackice999 <adam596341@yahoo.com>
2015-01-03 20:01:56 +0200

    Add comments

--- js/general.js
+++ js/general.js
@@ -79,18 +79,50 @@ function closePopup() {
 
 function showNotifications()
 {
+    //On email address click
     $('.registered-users').click(function()
     {
+       //Add class 'not-selected' from every list element
+       //except the selected one
+       //By this, only one list element (the selected) will be visible
        $('.registered-users').not(this).addClass('not-selected');
 
+        //If the numbering needs to be disabled, the below line does that
+        // $('.registered-users').css('list-style-type','none');
+
+       //Show the currently selected list element popup box
        $('.registered-users #show-notifications').show().index(this);
     });
+
+    //If the browser width is less than 700px
+    //Hide the add user form
+    if (window.innerWidth < 700)
+    {
+        document.getElementById('info-right-display').style.display = 'none';
+    }
 }
 
-function closeNotifications () {
+function closeNotifications ()
+{
+    //On email address click
     $('.registered-users').click(function(){
+
+        //Remove class 'not-selected' from every list element
+        //except the selected one
+        //By this, every list element will have its initial class structure
         $('.registered-users').not(this).removeClass('not-selected');
 
+        //If the numbering needs to be enabled, the below line does that
+        // $('.registered-users').css('list-style-type','decimal');
+
+        // Hide the currently selected list element popup box
         $('.registered-users #show-notifications').hide().index(this);
     });
+
+     //If the browser width is less than 700px
+    //Showw the add user form
+    if (window.innerWidth < 700)
+    {
+        document.getElementById('info-right-display').style.display = 'block';
+    }
 }
