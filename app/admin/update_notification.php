dbe5d7bf4235d684a6ff4f86a0a929e111aa2f39
blackice999 <adam596341@yahoo.com>
2015-01-14 20:07:59 +0200

    Add minimal styling and error throwing if the user isn't the admin

---
+++ app/admin/update_notification.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require __DIR__ . '/../database/database.php'; ?>
+    <title><?php echo $site_name;?> - Update note</title>
+    <link rel="stylesheet" type="text/css" href="../../css/main.css">
+</head>
+<body>
+    <div id="banner">
+        <img src="../../design/logo_four.png">
+        <!-- <span><?php echo $site_name; ?></span> -->
+    </div>
+
+    <div id="navigation">
+         <a href="../../logout.php" id="logout-right">Log out </a>
+         <a href="display_users.php" id="logout-right"> Go back </a>
+    </div>
+</body>
+</html>
+
+<?php
+    session_start();
+
+     //Throws an error if there isn't a logged person,
+    //and he isn't an admin (having ID 1)
+    if (!(isset($_SESSION['id']) && $_SESSION['id'] == 1))
+    {
+        throw new Exception("Unauthorized access!");
+    }
+?>
