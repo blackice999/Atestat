3e39ef77f8479b1e40e63fb8840c9ee1c98081e7
blackice999 <adam596341@yahoo.com>
2015-01-14 19:38:31 +0200

    Change heading size to fit the add note better in popup

--- app/admin/view_notifications.php
+++ app/admin/view_notifications.php
@@ -83,7 +83,7 @@
         <?php endwhile; ?>
     </ol>
     <div class="add-note">
-        <h2>Add note</h2>
+        <h3>Add note</h3>
         <form action="add_note.php" method="post" class="form-notification">
             <textarea name="note"></textarea> <br />
             <input type="submit" value="Add note" class="align-center">
