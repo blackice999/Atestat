c6b0805659c47b59f28b24facc2d997a82124287
blackice999 <adam596341@yahoo.com>
2015-01-15 19:19:15 +0200

    Add check if the received ID exists

--- app/admin/delete_notification.php
+++ app/admin/delete_notification.php
@@ -16,5 +16,32 @@
         {
             throw new Exception("Missing ID");
         }
+
+        //Sanitize the received ID
+        //removing all characters except digits, plus and minus sign
+        $filter_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+        $db = new Database();
+
+        //Specify which variables to bind
+        $bindArray = array(
+            'bindTypes' => 'i',
+            'bindVariables' => array(&$filter_id)
+            );
+
+        //Select the element having the received ID
+        //if the ID doesn't exist, null is returned
+        $valid_id = $db->bindQuery("SELECT `ID` from `user_notes` WHERE `ID` = ?", $bindArray);
+
+        //Gets the result in an array
+        $arrayID = $db->getArray($valid_id);
+
+        //If the ID cannot be found,
+        //redirect to the previous page, showing an error
+        if (!$array_ID)
+        {
+            header("Location: view_notifications.php?action=error");
+            die();
+        }
     }
 ?>
