7df7e381da196d3380e7be359ac78b6d40f56913
blackice999 <adam596341@yahoo.com>
2015-01-16 20:06:57 +0200

    Add changes to load correctly because the file is one directory higher now

--- app/admin/notifications/add_notification.php
+++ app/admin/notifications/add_notification.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php require __DIR__ . '/../database/database.php'; ?>
+    <?php require __DIR__ . '/../../database/database.php'; ?>
     <title><?php echo $site_name;?> - Add note</title>
-    <link rel="stylesheet" type="text/css" href="../../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/main.css">
 </head>
 <body>
     <div id="banner">
-        <img src="../../design/logo_four.png">
+        <img src="../../../design/logo_four.png">
         <!-- <span><?php echo $site_name; ?></span> -->
     </div>
 
     <div id="navigation">
-         <a href="../../logout.php" id="logout-right">Log out </a>
-         <a href="display_users.php" id="logout-right"> Go back </a>
+         <a href="../../../logout.php" id="logout-right">Log out </a>
+         <a href="../display_users.php" id="logout-right"> Go back </a>
     </div>
 </body>
 </html>
