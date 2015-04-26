e6042b4da823738bf23005a58dda2d4d074d7ca4
blackice999 <adam596341@yahoo.com>
2015-04-26 19:45:34 +0300

    Add error image

---
+++ error_page.php
@@ -0,0 +1,21 @@
+<?php 
+    include 'app/database/database.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $site_name; ?> - Error</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+     <div id="banner">
+        <img src="design/logo.png">
+        <!-- <span><?php echo $site_name; ?></span> -->
+    </div>
+
+    <div id="error_image">
+        <img src="design/dolphin_black.png">
+        <div id="error_text">
+    </div>
+</body>
+</html>
