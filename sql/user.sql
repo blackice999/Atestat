a6a91176e45f159085d14d37c425774ca13b182e
blackice999 <adam596341@yahoo.com>
2015-06-07 19:32:59 +0300

    Add default and new user, admin

--- sql/user.sql
+++ sql/user.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `user`
 (
     `ID` INT(4) NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(100) NOT NULL,
-    `statusID` INT(4) NOT NULL,
+    `statusID` INT(4) NOT NULL DEFAULT 1,
     `password` VARCHAR(255) NOT NULL,
     `password_hash` VARCHAR(255) NOT NULL,
     `date_registered` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -14,3 +14,7 @@ CREATE TABLE IF NOT EXISTS `user`
     FOREIGN KEY(`statusID`)
         REFERENCES user_status(`ID`)
 ) ENGINE=INNODB;
+
+
+INSERT INTO `user` (`email`, `password`)
+    VALUES('admin@prototype.com', '$2y$10$LhBsAfP7prznlErnqDo3EuE5eeH8qPEn6ZgaNoNQkgVzRFaxurI0e')
